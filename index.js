bdbc87c71f8389141a62b0121817e37e262a8b23
Money Penny <pennyquirino@gmail.com>
2021-02-09 15:51:22 -0500

    adding modules changed questions without validation to see if code would work.

--- index.js
+++ index.js
@@ -1,54 +1,38 @@
 // create consts for fs and inquirer
-const fs = require("fs");
+
 const inquirer = require("inquirer");
 
 
 
 // questions to prompt user to make a readme file
 
-function promptUser(){
-    return inquirer.prompt([
+inquirer
+    .prompt([
         {
             type: "input",
             name: "projectTitle",
             message: "What is the title of your project?",
-            validate: function (answer) {
-                if (answer.length < 1) {
-                    return console.log("You must enter a title name to continue");
-                }
-                return true;
-            }
+           
         },
         {
             type: "input",
             name: "description",
             message: "Describe your project including wins and struggles encountered:",
-            validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("You must enter a description of your project to continue");
-            }
-            return true;
+           
         },
-        
+        {
             type: "input",
             name: "installation",
             message: "What were the steps to get the development environment running?",
-            validate: function (answer) {
-                if (answer.length < 1) {
-                    return console.log("You must enter your installation steps to continue");
-                }
-                return true;
+            
             },
-        
+        {
             type: "input",
             name: "credits",
             message: "List your collaborators, if any with links to their GitHub accounts",
-            validate: function (answer) {
-                if (answer.length < 1) {
-                    return console.log("Please enter any collaborators to continue or N/A");
-                }
-                return true;
+          
             },
+            {
 
             type: "input",
             name: "license",
@@ -60,16 +44,17 @@ function promptUser(){
                 return true;
             }
         }
+    
+        
     ]);
-} 
 // create a function to write the file 
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("You have successfully generated your readme file")
-    });
-}
+// function writeToFile(fileName, data) {
+//     fs.writeFile(fileName, data, err => {
+//         if (err) {
+//             return console.log(err);
+//         }
+//         console.log("You have successfully generated your readme file")
+//     });
+// }
 
 // need a promise and need to write the main function of this program
