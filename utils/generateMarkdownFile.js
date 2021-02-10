41f0154347ec1120149fc68ceb232ed6b5ee36a2
Money Penny <pennyquirino@gmail.com>
2021-02-09 21:06:50 -0500

    added code to license badge and the function to generate the license section if any

--- utils/generateMarkdownFile.js
+++ utils/generateMarkdownFile.js
@@ -1,11 +1,28 @@
 // create function to generate license if present
-
+function renderLicenseBadge(license){
+    switch(license) {
+        case "MIT":
+            return "https://img.shields.io/badge/License-MIT-blue.svg";
+        case "Unlicense":
+            return "https://img.shields.io/badge/License-unlicense-blue.svg";
+        case "Apache 2.0":
+            return "https://img.shields.io/badge/License-Apache%202.0-blue.svg";
+        case "Simple 2.0":
+            return "https://img.shields.io/badge/License-simple-blue.svg";
+        case "BSD 2 Clause":
+            return "https://img.shields.io/badge/License-BSD%202--Clause-blue.svg";
+        
+              }
+          }
 
 
 // create function to get license URL
 
 
 // create function to get license section of file if any
+function renderLicenseSection(license) {
+    return `# ${data.projectTitle} is released under ${license} and is open source`;
+}
 
 
 // creat function to generate the README file
