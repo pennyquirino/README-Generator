096f2866c66febc41f79553ce40cb876f687fdac
Money Penny <pennyquirino@gmail.com>
2021-02-09 20:56:40 -0500

    added comments to list what is needed

--- utils/generateMarkdownFile.js
+++ utils/generateMarkdownFile.js
@@ -1,3 +1,21 @@
+// create function to generate license if present
+
+
+
+// create function to get license URL
+
+
+// create function to get license section of file if any
+
+
+// creat function to generate the README file
+
+
+// Table of Contents
+
+
+
+
 ## Table of Contents
 * [Installation](#installation)
 * [Credits](#credits)
@@ -15,3 +33,7 @@ ${data.credits}
 
 ## License
 ${data.license}
+
+
+
+// don't forget module exports
