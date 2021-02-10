bf1f8e326372376fa055153ef61f395f2a25d688
Money Penny <pennyquirino@gmail.com>
2021-02-09 20:22:16 -0500

    added functions for prompting questions, writeToFile, and the initialized functions created const for answer and markdown

--- index.js
+++ index.js
@@ -48,10 +48,24 @@ const questions = [
     ];
 
     // need to place a function here to promt user with questions
-
+function promptQuestion(questions) {
+    return inquirer.prompt(questions);
+}
 
     // place function here to write the file after questions are answered by user
-
+function writeToFile(fileName, data) {
+    writeFileASync(fileName, data);
+}
 
 
     // figure out how to init this so that it actually does something
+const init = function (async) {
+    try {
+        const answer = await promptQuestion(questions);
+
+        const md = await generateMarkdownFile(answer);
+
+        writeToFile("README.md", md);
+        
+    }
+}
